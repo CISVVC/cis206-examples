68dabd7d5cc022094c594a6c449dbedd90d14da7
Paul Tonning <paul.tonning@student.vvc.edu>
2019-02-21 08:23:31 -0800

    udate

---
+++ hello-world/Main.java
@@ -0,0 +1,16 @@
+/*
+ * file: Main.java
+ * description: My first java program
+ * developer: Your Name
+ * email: youremail@student.vvc.edu
+ */
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+
+      System.out.println("Hello World");
+
+   }
+}
