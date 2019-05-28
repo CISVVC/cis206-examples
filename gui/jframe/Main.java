e220f55641f0045bb1fff6d0afbcd4ee6bdd5ed8
Paul Tonning <paul.tonning@student.vvc.edu>
2019-05-28 12:41:27 -0700

    add gui example

---
+++ gui/jframe/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+public class Main
+{
+    public static void main(String [] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setTitle("My Awesome Frame");
+        frame.setSize(640,480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
