523a1472f7b6abe6a05af8aadfa2fedd6646a1b5
Paul Tonning <paul.tonning@student.vvc.edu>
2020-03-31 10:28:53 -0700

    update

--- methods/blackbox/Main.java
+++ methods/blackbox/Main.java
@@ -1,17 +1,32 @@
 public class Main
 {
 
-   
+/**
+ * This method is used to return the square of the given number
+ * @param x this is the number to square
+ * @return double this returns the square of the given number
+ */
    public static double square(double x)
    {
       return x * x;
    }
 
+/**
+ * This method is used to return the cube of the given number
+ * @param x this is the number to cube
+ * @return double this returns the cube of the given number
+ */
    public static double cube(double x)
    {
       return x * x * x;
    }
 
+/**
+ * This method is adds 2 given integers
+ * @param x this is the first integer
+ * @param y this is the second integer
+ * @return int this returns the sum of the 2 given numbers
+ */
    public static int add(int x,int y) {
         return x + y;
    }
@@ -20,9 +35,11 @@ public class Main
    {
 
 
+      System.out.printf("value\tsquare\tcube\tvalue+(value+2)\n");
       for(int i=1;i<10;i++)
       {
-         System.out.printf("%d\t%d\n",(int)square(i),(int)cube(i));
+         System.out.printf("%d\t%d\t%d\t%d\n",
+                           (int)i,(int)square(i),(int)cube(i),add(i,i+2));
       }
 
    }
