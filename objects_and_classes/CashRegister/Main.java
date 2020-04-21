248d91ce503243990e1ec0f1182a3842955a2473
Paul Tonning <paul.tonning@student.vvc.edu>
2020-04-21 12:05:37 -0700

    update

--- objects_and_classes/CashRegister/Main.java
+++ objects_and_classes/CashRegister/Main.java
@@ -1,17 +1,20 @@
 public class Main {
 
    public static void display(CashRegister reg) {
-      System.out.printf("Item %d: $%.2f\n",reg.get_count(),reg.get_total());
+      System.out.printf("Item %d: $%.2f\n",reg.getCount(),reg.getTotal());
    }
    
    public static void main(String arg[]) {
       CashRegister register1 = new CashRegister();
+      CashRegister register2 = new CashRegister();
+      System.out.println(register1);
+      System.out.println(register2);
       register1.clear();
-      register1.add_item(1.95);
+      register1.addItem(1.95);
       display(register1);
-      register1.add_item(0.95);
+      register1.addItem(0.95);
       display(register1);
-      register1.add_item(2.50);
+      register1.addItem(2.50);
       display(register1);
    }
 
